0bcaf64bb5401dee8e355e1b574efeaa10eacba6
mmbukane-shy6 <sheilaegeidza@gmail.com>
2020-05-24 17:35:36 +0300

    added templates folder

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,26 @@
-from django.http  import HttpResponse
+from django.shortcuts import render,redirect
+# from django import HttpResponse
+from .models import Image, Category, Location
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the My Gallery')
+def index(request):
+    return render(request, 'index.html')
+
+def gallery(request):
+    images = Image.objects.all()
+    categories = Category.objects.all()
+    location = Location.objects.all()
+    return render(request, 'gallery.html', locals())
+
+def search_category(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_image_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'category.html', {"message": message, "images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'category.html', {"message": message})
+
